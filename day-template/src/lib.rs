05a2b67a6a53b89071343dbb1675436e928208e0
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2020-11-27 23:03:12 +0100

    fix templates
    tinytemplate doesn't need to escape unopened trailing closing
    braces, apparently.

--- day-template/src/lib.rs
+++ day-template/src/lib.rs
@@ -3,13 +3,13 @@ use aoc2020::parse;
 use std::path::Path;
 use thiserror::Error;
 
-pub fn part1(input: &Path) -> Result<(), Error> {
+pub fn part1(input: &Path) -> Result<(), Error> \{
     unimplemented!()
 }
 
-pub fn part2(_input: &Path) -> Result<(), Error> {
+pub fn part2(_input: &Path) -> Result<(), Error> \{
     unimplemented!()
 }
 
 #[derive(Debug, Error)]
-pub enum Error {}
+pub enum Error \{}
