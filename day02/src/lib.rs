487124b5578573d38c32aaa14a92d7a16d89a665
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2020-12-02 15:55:58 +0100

    refactor: single struct

--- day02/src/lib.rs
+++ day02/src/lib.rs
@@ -4,39 +4,33 @@ use std::path::Path;
 use thiserror::Error;
 
 #[derive(parse_display::Display, parse_display::FromStr)]
-#[display("{min_count}-{max_count} {char_counted}")]
+#[display("{min_count}-{max_count} {char_counted}: {example}")]
 struct PasswordPolicy {
     min_count: u32,
     max_count: u32,
     char_counted: char,
-}
-
-#[derive(parse_display::Display, parse_display::FromStr)]
-#[display("{policy}: {example}")]
-struct PasswordExample {
-    policy: PasswordPolicy,
     example: String,
 }
 
-impl PasswordExample {
+impl PasswordPolicy {
     fn is_valid(&self) -> bool {
         let ch_count = self
             .example
             .chars()
-            .filter(|ch| *ch == self.policy.char_counted)
+            .filter(|ch| *ch == self.char_counted)
             .count();
-        ch_count >= self.policy.min_count as usize && ch_count <= self.policy.max_count as usize
+        ch_count >= self.min_count as usize && ch_count <= self.max_count as usize
     }
 
     fn is_valid_part2(&self) -> bool {
         let example = self.example.as_bytes();
-        (example[self.policy.min_count as usize - 1] == self.policy.char_counted as u8)
-            ^ (example[self.policy.max_count as usize - 1] == self.policy.char_counted as u8)
+        (example[self.min_count as usize - 1] == self.char_counted as u8)
+            ^ (example[self.max_count as usize - 1] == self.char_counted as u8)
     }
 }
 
 pub fn part1(input: &Path) -> Result<(), Error> {
-    let n_valid = parse::<PasswordExample>(input)?
+    let n_valid = parse::<PasswordPolicy>(input)?
         .filter(|example| example.is_valid())
         .count();
     println!("{} valid passwords", n_valid);
@@ -44,7 +38,7 @@ pub fn part1(input: &Path) -> Result<(), Error> {
 }
 
 pub fn part2(input: &Path) -> Result<(), Error> {
-    let n_valid = parse::<PasswordExample>(input)?
+    let n_valid = parse::<PasswordPolicy>(input)?
         .filter(|example| example.is_valid_part2())
         .count();
     println!("{} valid passwords (part 2)", n_valid);
