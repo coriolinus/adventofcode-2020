dc6707fe07a51a96e79dec7a87a836cfb01b8384
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2020-12-02 15:24:39 +0100

    refactor: use parse_display instead of manual impl

--- day02/src/lib.rs
+++ day02/src/lib.rs
@@ -1,71 +1,21 @@
 use aoc2020::parse;
 
 use std::path::Path;
-use std::str::FromStr;
 use thiserror::Error;
 
+#[derive(parse_display::Display, parse_display::FromStr)]
+#[display("{min_count}-{max_count} {char_counted}")]
 struct PasswordPolicy {
     min_count: u32,
     max_count: u32,
     char_counted: char,
 }
 
-impl FromStr for PasswordPolicy {
-    type Err = String;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let space_parts: Vec<_> = s.split(' ').collect();
-        if space_parts.len() != 2 {
-            Err(format!("expected 2 space_parts; got {}", space_parts.len()))?;
-        }
-        let num_parts: Vec<_> = space_parts[0].split('-').collect();
-        if num_parts.len() != 2 {
-            Err(format!("expected 2 num_parts; got {}", num_parts.len()))?;
-        }
-
-        let min_count = num_parts[0].parse::<u32>().map_err(|err| err.to_string())?;
-        let max_count = num_parts[1].parse::<u32>().map_err(|err| err.to_string())?;
-        let mut char_policy_chars = space_parts[1].chars();
-        let char_counted = char_policy_chars
-            .next()
-            .ok_or(format!("no char in space_parts[1]"))?;
-        if let Some(c) = char_policy_chars.next() {
-            Err(format!(
-                "expected a single char in char_policy; found extra '{}'",
-                c
-            ))?;
-        }
-
-        Ok(PasswordPolicy {
-            min_count,
-            max_count,
-            char_counted,
-        })
-    }
-}
-
+#[derive(parse_display::Display, parse_display::FromStr)]
+#[display("{policy}: {example}")]
 struct PasswordExample {
     policy: PasswordPolicy,
     example: String,
-}
-
-impl FromStr for PasswordExample {
-    type Err = String;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let parts: Vec<_> = s.split(": ").collect();
-        if parts.len() != 2 {
-            Err(format!(
-                "expected 2 passwordexample parts; got {}",
-                parts.len()
-            ))?;
-        }
-
-        let policy = parts[0].parse()?;
-        let example = parts[1].to_string();
-
-        Ok(PasswordExample { policy, example })
-    }
 }
 
 impl PasswordExample {
