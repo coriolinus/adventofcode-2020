b7aac28f6eed0a6ee51a26c80a3dc707dd42d0b4
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2020-12-04 10:31:00 +0100

    day 4 part 2: fixed
    Not certain, but the problem was almost certainly the fact that I'd
    previously failed to ensure the full match in the regexes. The rest
    of the changes in this set were just cleanups to make the code a bit
    nicer.

--- day04/src/lib.rs
+++ day04/src/lib.rs
@@ -8,9 +8,9 @@ use std::str::FromStr;
 use thiserror::Error;
 
 lazy_static! {
-    static ref HAIR_COLOR_RE: Regex = Regex::new(r"#[0-9a-f]{6}").unwrap();
-    static ref EYE_COLOR_RE: Regex = Regex::new(r"(amb|blu|brn|gry|grn|hzl|oth)").unwrap();
-    static ref PASSPORT_ID_RE: Regex = Regex::new(r"\d{9}").unwrap();
+    static ref HAIR_COLOR_RE: Regex = Regex::new(r"^#[0-9a-f]{6}$").unwrap();
+    static ref EYE_COLOR_RE: Regex = Regex::new(r"^(amb|blu|brn|gry|grn|hzl|oth)$").unwrap();
+    static ref PASSPORT_ID_RE: Regex = Regex::new(r"^\d{9}$").unwrap();
 }
 
 #[derive(Default)]
@@ -26,53 +26,41 @@ impl PassportRaw {
             .all(|&field| self.data.contains_key(field))
     }
 
-    fn parse_numeric_field(&self, field: &str, min: u32, max: u32) -> bool {
-        let field_data = match self.data.get(field) {
-            Some(field) => field,
-            None => return false,
-        };
+    fn parse_numeric_field(&self, field: &str, min: u32, max: u32) -> Option<bool> {
+        let field = self.data.get(field)?;
+        let field = field.parse::<u32>().ok()?;
 
-        let field_num = match field_data.parse::<u32>() {
-            Ok(n) => n,
-            Err(_) => return false,
-        };
-
-        field_num >= min && field_num <= max
+        Some(field >= min && field <= max)
     }
 
-    fn is_valid_height(&self) -> bool {
-        let hgt = match self.data.get("hgt") {
-            Some(field) => field,
-            None => return false,
-        };
-        let hgt: Height = match hgt.parse() {
-            Ok(hgt) => hgt,
-            Err(_) => return false,
-        };
+    fn is_valid_height(&self) -> Option<bool> {
+        let hgt = self.data.get("hgt")?;
+        let hgt: Height = hgt.parse().ok()?;
 
-        match hgt {
+        Some(match hgt {
             Height::Cm(cm) => cm >= 150 && cm <= 193,
             Height::In(inch) => inch >= 59 && inch <= 76,
-        }
+        })
     }
 
-    fn is_valid_re(&self, field: &str, re: &Regex) -> bool {
-        let field = match self.data.get(field) {
-            Some(field) => field,
-            None => return false,
-        };
-
-        re.is_match(field)
+    fn is_valid_re(&self, field: &str, re: &Regex) -> Option<bool> {
+        let field = self.data.get(field)?;
+        Some(re.is_match(field))
     }
 
     fn is_valid(&self) -> bool {
         self.parse_numeric_field("byr", 1920, 2002)
-            && self.parse_numeric_field("iyr", 2010, 2020)
-            && self.parse_numeric_field("eyr", 2020, 2030)
-            && self.is_valid_height()
-            && self.is_valid_re("hcl", &HAIR_COLOR_RE)
-            && self.is_valid_re("ecl", &EYE_COLOR_RE)
-            && self.is_valid_re("pid", &PASSPORT_ID_RE)
+            .unwrap_or_default()
+            && self
+                .parse_numeric_field("iyr", 2010, 2020)
+                .unwrap_or_default()
+            && self
+                .parse_numeric_field("eyr", 2020, 2030)
+                .unwrap_or_default()
+            && self.is_valid_height().unwrap_or_default()
+            && self.is_valid_re("hcl", &HAIR_COLOR_RE).unwrap_or_default()
+            && self.is_valid_re("ecl", &EYE_COLOR_RE).unwrap_or_default()
+            && self.is_valid_re("pid", &PASSPORT_ID_RE).unwrap_or_default()
     }
 }
 
