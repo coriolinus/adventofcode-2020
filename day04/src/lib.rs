e8b751595c73f72f86902f37797bc4b325a8bd40
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2020-12-04 10:14:46 +0100

    day 4 part 2: wrong
    My guess is 148 given this setup, and it is not at all clear why
    the site reports this as too high. All valid and invalid examples
    on the page are sorted appropriately. Looking into it.

--- day04/src/lib.rs
+++ day04/src/lib.rs
@@ -1,9 +1,17 @@
 use aoc2020::input::parse_newline_sep;
 
+use lazy_static::lazy_static;
+use regex::Regex;
 use std::collections::HashMap;
 use std::path::Path;
 use std::str::FromStr;
 use thiserror::Error;
+
+lazy_static! {
+    static ref HAIR_COLOR_RE: Regex = Regex::new(r"#[0-9a-f]{6}").unwrap();
+    static ref EYE_COLOR_RE: Regex = Regex::new(r"(amb|blu|brn|gry|grn|hzl|oth)").unwrap();
+    static ref PASSPORT_ID_RE: Regex = Regex::new(r"\d{9}").unwrap();
+}
 
 #[derive(Default)]
 struct PassportRaw {
@@ -18,8 +26,53 @@ impl PassportRaw {
             .all(|&field| self.data.contains_key(field))
     }
 
+    fn parse_numeric_field(&self, field: &str, min: u32, max: u32) -> bool {
+        let field_data = match self.data.get(field) {
+            Some(field) => field,
+            None => return false,
+        };
+
+        let field_num = match field_data.parse::<u32>() {
+            Ok(n) => n,
+            Err(_) => return false,
+        };
+
+        field_num >= min && field_num <= max
+    }
+
+    fn is_valid_height(&self) -> bool {
+        let hgt = match self.data.get("hgt") {
+            Some(field) => field,
+            None => return false,
+        };
+        let hgt: Height = match hgt.parse() {
+            Ok(hgt) => hgt,
+            Err(_) => return false,
+        };
+
+        match hgt {
+            Height::Cm(cm) => cm >= 150 && cm <= 193,
+            Height::In(inch) => inch >= 59 && inch <= 76,
+        }
+    }
+
+    fn is_valid_re(&self, field: &str, re: &Regex) -> bool {
+        let field = match self.data.get(field) {
+            Some(field) => field,
+            None => return false,
+        };
+
+        re.is_match(field)
+    }
+
     fn is_valid(&self) -> bool {
-        self.is_northpole() && self.data.contains_key("cid")
+        self.parse_numeric_field("byr", 1920, 2002)
+            && self.parse_numeric_field("iyr", 2010, 2020)
+            && self.parse_numeric_field("eyr", 2020, 2030)
+            && self.is_valid_height()
+            && self.is_valid_re("hcl", &HAIR_COLOR_RE)
+            && self.is_valid_re("ecl", &EYE_COLOR_RE)
+            && self.is_valid_re("pid", &PASSPORT_ID_RE)
     }
 }
 
@@ -48,16 +101,41 @@ impl FromStr for PassportRaw {
     }
 }
 
+enum Height {
+    Cm(u32),
+    In(u32),
+}
+
+impl FromStr for Height {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        if s.ends_with("cm") {
+            let n = s[..s.len() - 2].parse::<u32>().map_err(|e| e.to_string())?;
+            Ok(Self::Cm(n))
+        } else if s.ends_with("in") {
+            let n = s[..s.len() - 2].parse::<u32>().map_err(|e| e.to_string())?;
+            Ok(Self::In(n))
+        } else {
+            Err("unrecognized measurement".into())
+        }
+    }
+}
+
 pub fn part1(input: &Path) -> Result<(), Error> {
     let valid = parse_newline_sep::<PassportRaw>(input)?
         .filter(|pr| pr.is_northpole())
         .count();
-    println!("valid (northpole): {}", valid);
+    println!("count (northpole): {}", valid);
     Ok(())
 }
 
-pub fn part2(_input: &Path) -> Result<(), Error> {
-    unimplemented!()
+pub fn part2(input: &Path) -> Result<(), Error> {
+    let valid = parse_newline_sep::<PassportRaw>(input)?
+        .filter(|pr| pr.is_valid())
+        .count();
+    println!("valid: {}", valid);
+    Ok(())
 }
 
 #[derive(Debug, Error)]
