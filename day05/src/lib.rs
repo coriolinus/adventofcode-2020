6d7868e3a962c4ab9c8279fc126e3b6c8eee6eb6
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2020-12-05 12:36:39 +0100

    day 05 part 2

--- day05/src/lib.rs
+++ day05/src/lib.rs
@@ -51,12 +51,56 @@ pub fn part1(input: &Path) -> Result<(), Error> {
     Ok(())
 }
 
-pub fn part2(_input: &Path) -> Result<(), Error> {
-    unimplemented!()
+fn find_empty_seat_id(map: &[bool]) -> Option<usize> {
+    for (left_idx, window) in map.windows(3).enumerate() {
+        if window[0] && !window[1] && window[2] {
+            return Some(left_idx + 1);
+        }
+    }
+    None
+}
+
+pub fn part2(input: &Path) -> Result<(), Error> {
+    let mut map = vec![false; 128 * 8];
+    for boarding_pass in parse::<BinarySpacePartition>(input)? {
+        map[boarding_pass.seat_id() as usize] = true;
+    }
+
+    let empty_seat_id = find_empty_seat_id(&map);
+    println!("empty seat id: {:?}", empty_seat_id);
+
+    Ok(())
 }
 
 #[derive(Debug, Error)]
 pub enum Error {
     #[error(transparent)]
     Io(#[from] std::io::Error),
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    fn check_seat(seat: &str, expect_row: u8, expect_col: u8, expect_id: u32) {
+        let boarding_pass: BinarySpacePartition = seat.parse().unwrap();
+        assert_eq!(boarding_pass.row(), expect_row);
+        assert_eq!(boarding_pass.col(), expect_col);
+        assert_eq!(boarding_pass.seat_id(), expect_id);
+    }
+
+    #[test]
+    fn example_1() {
+        check_seat("BFFFBBFRRR", 70, 7, 567);
+    }
+
+    #[test]
+    fn example_2() {
+        check_seat("FFFBBBFRRR", 14, 7, 119);
+    }
+
+    #[test]
+    fn example_3() {
+        check_seat("BBFFBBFRLL", 102, 4, 820);
+    }
 }
