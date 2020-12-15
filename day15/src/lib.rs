fe1665e2e4ddf0d14bafe04eafb07674deb3ef33
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2020-12-15 10:27:38 +0100

    use vectors as cache lookup
    Turns out that hashing is expensive! Part 2 now runs in ~0.8 seconds,
    vice the ~2.6 it previously required with the HashMap implementation.

--- day15/src/lib.rs
+++ day15/src/lib.rs
@@ -1,28 +1,41 @@
 use aoc2020::{parse, CommaSep};
 
-use std::{collections::HashMap, path::Path};
+use std::path::Path;
 use thiserror::Error;
 
 fn memory_game(initializers: &[u32]) -> impl '_ + Iterator<Item = u32> {
     let mut turn = 0;
     let mut previous = 0;
-    let mut most_recent = HashMap::<u32, usize>::new();
-    let mut second_most_recent = HashMap::<u32, usize>::new();
+    let mut most_recent = Vec::new();
+    let mut second_most_recent = Vec::new();
 
     std::iter::from_fn(move || {
         let current = if turn < initializers.len() {
             initializers[turn]
         } else {
             second_most_recent
-                .get(&previous)
-                .map(|&second_most| (turn - second_most) as u32)
+                .get(previous as usize)
+                .map(|&second_most| {
+                    if second_most != 0 {
+                        (turn - second_most) as u32
+                    } else {
+                        0
+                    }
+                })
                 .unwrap_or_default()
         };
         turn += 1;
-        if let Some(most_recent) = most_recent.get(&current) {
-            second_most_recent.insert(current, *most_recent);
+        let ucurrent = current as usize;
+        if most_recent.len() <= ucurrent {
+            most_recent.resize(ucurrent + 1, 0);
         }
-        most_recent.insert(current, turn);
+        if most_recent[ucurrent] != 0 {
+            if second_most_recent.len() <= ucurrent {
+                second_most_recent.resize(ucurrent + 1, 0);
+            }
+            second_most_recent[ucurrent] = most_recent[ucurrent]
+        }
+        most_recent[ucurrent] = turn;
         previous = current;
         Some(current)
     })
@@ -63,6 +76,7 @@ mod test {
         let initializers = [0, 3, 6];
         let expect = [0_u32, 3, 6, 0, 3, 3, 1, 0, 4, 0];
         for (actual, expect) in memory_game(&initializers).zip(&expect) {
+            dbg!(actual, expect);
             assert_eq!(actual, *expect);
         }
     }
