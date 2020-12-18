30c07d37b3c56428ddb86f6c33545f0669d9096c
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2020-12-18 10:39:22 +0100

    solve part 2
    TBH, kind of surprised that this just worked first try.
    Not sure how I feel about the extensibility today. On the one hand,
    refactoring the whole AST and parser to change the operator
    precedence is probably the "right" answer. On the other hand,
    retaining a flat operator structure and handling the varying
    precedence in code was simpler to implement today, with less
    duplication of work. On the gripping hand, it would have been a
    huge pain to handle the prededence like this if there were more than
    two operations; in that case, I'd just need to do the proper tree
    structure regardless.
    
    Given the rarity with which I'll be designing a language whose
    operator precedence changes out from under me, I think it's fine
    not to have an elegant strategy in hand for dealing with that
    circumstance.

--- day18/src/lib.rs
+++ day18/src/lib.rs
@@ -47,6 +47,14 @@ impl Value {
             Value::Expression(e) => e.value(),
         }
     }
+
+    // reduce this value to a literal using advanced math
+    fn reduce_advanced(&self) -> i64 {
+        match self {
+            Value::Literal(n) => *n,
+            Value::Expression(expr) => expr.evaluate_advanced(),
+        }
+    }
 }
 
 impl<T> From<T> for Value
@@ -95,8 +103,38 @@ impl FromStr for Expr {
 }
 
 impl Expr {
+    fn evaluate(terms: &[Term]) -> i64 {
+        terms.iter().fold(0, |acc, elem| elem.apply_to(acc))
+    }
+
     pub fn value(&self) -> i64 {
-        self.terms.iter().fold(0, |acc, elem| elem.apply_to(acc))
+        Self::evaluate(&self.terms)
+    }
+
+    /// In "advanced" math, addition is evaluated _before_ multiplication.
+    pub fn evaluate_advanced(&self) -> i64 {
+        // convert to a list of multiplications
+        let remaining_terms = self.terms.iter().fold(Vec::new(), |mut remaining, term| {
+            // first term
+            if remaining.is_empty() {
+                remaining.push(term.value.reduce_advanced());
+                return remaining;
+            }
+
+            match term.operation {
+                Operation::Add => {
+                    // fold this term's value into the final term of remaining
+                    let last = remaining.len() - 1;
+                    remaining[last] += term.value.reduce_advanced();
+                }
+                Operation::Mul => {
+                    remaining.push(term.value.reduce_advanced());
+                }
+            }
+            remaining
+        });
+        // evaluate that list
+        remaining_terms.iter().product()
     }
 }
 
@@ -106,8 +144,12 @@ pub fn part1(input: &Path) -> Result<(), Error> {
     Ok(())
 }
 
-pub fn part2(_input: &Path) -> Result<(), Error> {
-    unimplemented!()
+pub fn part2(input: &Path) -> Result<(), Error> {
+    let sum = parse::<Expr>(input)?
+        .map(|expr| expr.evaluate_advanced())
+        .sum::<i64>();
+    println!("sum of expressions (advanced): {}", sum);
+    Ok(())
 }
 
 #[derive(Debug, Error)]
