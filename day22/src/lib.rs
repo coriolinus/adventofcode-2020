afbf5a9b3a862e01bda0ec45093edbd8abc8c132
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2020-12-22 16:34:31 +0100

    implement part 2, but it's too slow

--- day22/src/lib.rs
+++ day22/src/lib.rs
@@ -1,13 +1,18 @@
 use aoc2020::input::parse_newline_sep;
 
 use regex::Regex;
-use std::{collections::VecDeque, path::Path, str::FromStr};
+use std::{
+    collections::{HashSet, VecDeque},
+    path::Path,
+    str::FromStr,
+};
 use thiserror::Error;
 
 lazy_static::lazy_static! {
     static ref PLAYER_RE: Regex = Regex::new(r"^Player (\d+):$").unwrap();
 }
 
+#[derive(Clone)]
 struct Player {
     id: u8,
     cards: VecDeque<u8>,
@@ -102,6 +107,133 @@ fn calculate_score_for(player1: &Player, player2: &Player, winner: u8) -> u32 {
         .sum()
 }
 
+/// play a round of "Recursive Combat", returning the winner, if any
+fn play_recursive(
+    player1: &mut Player,
+    player2: &mut Player,
+    next_game: &mut usize,
+    trace: bool,
+) -> u8 {
+    let game = *next_game;
+    let mut memory = HashSet::new();
+    if trace {
+        println!("=== Game {} ===", game);
+    }
+
+    if !memory.insert((player1.cards.clone(), player2.cards.clone())) {
+        // insert returns `false` when the set already contains the item
+        if trace {
+            println!("game state already reached; player {} wins", player1.id);
+        }
+        return player1.id;
+    }
+
+    let mut round = 0;
+
+    while !(player1.cards.is_empty() || player2.cards.is_empty()) {
+        round += 1;
+        if trace {
+            println!("-- Round {} (Game {}) --", round, game);
+            println!("player {}'s deck: {:?}", player1.id, player1.cards);
+            println!("player {}'s deck: {:?}", player2.id, player2.cards);
+        }
+
+        let card1 = match player1.cards.pop_front() {
+            Some(card) => card,
+            None => {
+                if trace {
+                    println!(
+                        "Player {} out of cards; player {} wins",
+                        player1.id, player2.id
+                    );
+                }
+                return player2.id;
+            }
+        };
+        let card2 = match player2.cards.pop_front() {
+            Some(card) => card,
+            None => {
+                if trace {
+                    println!(
+                        "Player {} out of cards; player {} wins",
+                        player2.id, player1.id
+                    );
+                }
+                return player1.id;
+            }
+        };
+
+        let winner;
+        let winner_card;
+        let loser_card;
+
+        if trace {
+            println!("Player 1 plays {}", card1);
+            println!("Player 2 plays {}", card2);
+        }
+
+        if player1.cards.len() >= card1 as usize && player2.cards.len() >= card2 as usize {
+            // play a complete sub-game to determine the winner of this round
+            if trace {
+                println!("Playing a sub-game to determine the winner...");
+            }
+
+            let mut sub_player1 = player1.clone();
+            sub_player1.cards.truncate(card1 as usize);
+            let mut sub_player2 = player2.clone();
+            sub_player2.cards.truncate(card2 as usize);
+
+            *next_game += 1;
+
+            if player1.id == play_recursive(&mut sub_player1, &mut sub_player2, next_game, trace) {
+                winner = &mut *player1;
+                winner_card = card1;
+                loser_card = card2;
+            } else {
+                winner = &mut *player2;
+                winner_card = card2;
+                loser_card = card1;
+            }
+
+            if trace {
+                println!("...anyway, back to game {}", game);
+            }
+        } else {
+            if card1 > card2 {
+                winner = &mut *player1;
+                winner_card = card1;
+                loser_card = card2;
+            } else {
+                winner = &mut *player2;
+                winner_card = card2;
+                loser_card = card1;
+            }
+        }
+
+        if trace {
+            println!(
+                "Player {} wins round {} of game {}!",
+                winner.id, round, game
+            );
+        }
+
+        winner.cards.push_back(winner_card);
+        winner.cards.push_back(loser_card);
+    }
+
+    if player1.cards.is_empty() {
+        if trace {
+            println!("The winner of game {} is player {}", game, player2.id);
+        }
+        player2.id
+    } else {
+        if trace {
+            println!("The winner of game {} is player {}", game, player1.id);
+        }
+        player1.id
+    }
+}
+
 pub fn part1(input: &Path) -> Result<(), Error> {
     let mut players: Vec<Player> = parse_newline_sep(input)?.collect();
     if players.len() != 2 {
@@ -116,8 +248,19 @@ pub fn part1(input: &Path) -> Result<(), Error> {
     Ok(())
 }
 
-pub fn part2(_input: &Path) -> Result<(), Error> {
-    unimplemented!()
+pub fn part2(input: &Path, trace: bool) -> Result<(), Error> {
+    let mut players: Vec<Player> = parse_newline_sep(input)?.collect();
+    if players.len() != 2 {
+        return Err(Error::WrongNumberPlayers(players.len()));
+    }
+    let mut player2 = players.swap_remove(1);
+    let mut player1 = players.swap_remove(0);
+    let mut next_game = 1;
+
+    let winner = play_recursive(&mut player1, &mut player2, &mut next_game, trace);
+    let score = calculate_score_for(&player1, &player2, winner);
+    println!("victor score (recursive): {}", score);
+    Ok(())
 }
 
 #[derive(Debug, Error)]
