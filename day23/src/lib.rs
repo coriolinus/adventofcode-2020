e72bd36f0b0459cc3fdb561b316ba3585f014a74
Peter Goodspeed-Niklaus <peter.r.goodspeedniklaus@gmail.com>
2020-12-23 17:59:33 +0100

    rewrite using vec as storage; add some tests

--- day23/src/lib.rs
+++ day23/src/lib.rs
@@ -1,13 +1,13 @@
 use aoc2020::parse;
 
-use std::{collections::HashMap, fmt, path::Path, str::FromStr};
+use std::{fmt, path::Path, str::FromStr};
 use thiserror::Error;
 
 #[derive(Clone, Default)]
 struct CupGame {
-    successors: HashMap<u32, u32>,
-    max: u32,
-    current: u32,
+    successors: Vec<usize>,
+    max: usize,
+    current: usize,
 }
 
 impl FromStr for CupGame {
@@ -18,13 +18,14 @@ impl FromStr for CupGame {
         let mut predecessor = 0;
         let mut n = 0;
         let mut game = CupGame::default();
+        game.successors.resize(s.chars().count() + 1, 0);
 
         for ch in s.chars() {
             n = ch.to_string().parse()?;
             if first.is_none() {
                 first = Some(n);
             }
-            game.successors.insert(predecessor, n);
+            game.successors[predecessor] = n;
             predecessor = n;
         }
         // we both insert and remove one element after this point,
@@ -34,12 +35,9 @@ impl FromStr for CupGame {
             return Err(Error::TooFewCups);
         }
 
-        // get rid of extraneous 0 element
-        game.successors.remove(&0);
-
         // we know that these must have been set, so just unwrap
-        game.successors.insert(n, first.unwrap());
-        game.max = game.successors.keys().copied().max().unwrap();
+        game.successors[n] = first.unwrap();
+        game.max = game.successors.iter().copied().max().unwrap();
         game.current = first.unwrap();
 
         Ok(game)
@@ -49,17 +47,17 @@ impl FromStr for CupGame {
 impl CupGame {
     fn turn(&mut self, trace: bool) {
         let mut stash = [0; 3];
-        stash[0] = self.successors[&self.current];
-        stash[1] = self.successors[&stash[0]];
-        stash[2] = self.successors[&stash[1]];
-        let subsequent = self.successors[&stash[2]];
+        stash[0] = self.successors[self.current];
+        stash[1] = self.successors[stash[0]];
+        stash[2] = self.successors[stash[1]];
+        let subsequent = self.successors[stash[2]];
 
         if trace {
             print!("cups: ({}) ", self.current);
-            let mut next = self.successors[&self.current];
+            let mut next = self.successors[self.current];
             while next != self.current {
                 print!("{} ", next);
-                next = self.successors[&next];
+                next = self.successors[next];
             }
             println!();
 
@@ -67,7 +65,7 @@ impl CupGame {
         }
 
         // excise the cup stash from the cycle
-        self.successors.insert(self.current, subsequent);
+        self.successors[self.current] = subsequent;
 
         // pick destination
         let mut destination = self.current - 1;
@@ -88,35 +86,37 @@ impl CupGame {
         }
 
         // place stash after destination
-        let after_destination = self.successors[&destination];
-        self.successors.insert(stash[2], after_destination);
-        self.successors.insert(destination, stash[0]);
+        let after_destination = self.successors[destination];
+        self.successors[stash[2]] = after_destination;
+        self.successors[destination] = stash[0];
 
         // update current
-        self.current = self.successors[&self.current];
+        self.current = self.successors[self.current];
     }
 
-    fn extend_to(&mut self, n: u32) {
-        let (prev, _current) = self
+    fn extend_to(&mut self, n: usize) {
+        let (mut prev, _current) = self
             .successors
             .iter()
+            .enumerate()
+            .skip(1)
             .find(|(_prev, &succ)| succ == self.current)
             .unwrap();
-        let mut prev = *prev;
+        self.successors.resize(n + 1, 0);
         for i in (self.max + 1)..=n {
-            self.successors.insert(prev, i);
+            self.successors[prev] = i;
             prev = i;
         }
-        self.successors.insert(n, self.current);
+        self.successors[n] = self.current;
     }
 }
 
 impl fmt::Display for CupGame {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let mut next = self.successors[&1];
+        let mut next = self.successors[1];
         while next != 1 {
             write!(f, "{}", next)?;
-            next = self.successors[&next];
+            next = self.successors[next];
         }
         Ok(())
     }
@@ -141,8 +141,8 @@ pub fn part2(input: &Path, trace: bool) -> Result<(), Error> {
         for _ in 0..10_000_000 {
             game.turn(trace);
         }
-        let first_successor = game.successors[&1];
-        let second_successor = game.successors[&first_successor];
+        let first_successor = game.successors[1];
+        let second_successor = game.successors[first_successor];
         let product = first_successor as u64 * second_successor as u64;
         println!(
             "input line {}: product of first 2 after 1 after ten million moves: {}",
@@ -171,5 +171,29 @@ mod tests {
         let mut game = CupGame::from_str("54321").unwrap();
         game.extend_to(10);
         assert_eq!(game.to_string(), "6789105432");
+    }
+
+    #[test]
+    fn test_extend_count() {
+        let mut game = CupGame::from_str("389125467").unwrap();
+        game.extend_to(1_000_000);
+
+        let mut count = 1;
+        let mut next = game.successors[1];
+        while next != 1 {
+            next = game.successors[next];
+            count += 1;
+        }
+
+        assert_eq!(count, 1_000_000);
+    }
+
+    #[test]
+    fn test_example() {
+        let mut game = CupGame::from_str("389125467").unwrap();
+        for _ in 0..100 {
+            game.turn(true);
+        }
+        assert_eq!(game.to_string(), "67384529");
     }
 }
